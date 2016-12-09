3ec048f77eb66ba43db92631a221ad2a24aec471
Paris Leach <parisleach@gmail.com>
2016-12-09 15:12:21 -0500

    setup server

---
+++ index.js
@@ -0,0 +1,13 @@
+require('dotenv').config({silent: true});
+const express = require('express');
+const logger = require('morgan');
+const path = require('path');
+
+const app = express();
+const PORT = process.argv[2] || process.env.PORT || 3000;
+
+app.use(logger('dev'));
+
+app.use(express.static(path.join(__dirname,'public')));
+
+app.listen(PORT, () => console.log('Server is listening on', PORT));
